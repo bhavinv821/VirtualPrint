cb64987023cd5ec9c5dcebd41ba4f96313ef8f24
bhavinv821 <bhavinv821@gmail.com>
2020-08-28 15:52:11 -0400

    Initial commit

---
+++ src/test/java/com/spring/virtualprint/VirtualXeroxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.virtualprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualXeroxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
